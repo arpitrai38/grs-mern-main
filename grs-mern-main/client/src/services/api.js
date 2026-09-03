65605868fa16e791e4ab9185492af307604f142f
Arpit Rai <sadhanamarendra12@gmail.com>
2026-09-03 12:24:12 +0530

    Update client API URL

--- grs-mern-main/client/src/services/api.js
+++ grs-mern-main/client/src/services/api.js
@@ -1,29 +1,38 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Adjust if backend PORT changes
+    baseURL: 'https://grs-mern-main.onrender.com/api',
 });
 
 // Request interceptor to add token
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = token;
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('token');
+
+        if (token) {
+            config.headers.Authorization = token;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
     }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
-});
+);
 
 // Response interceptor to handle unauthorized errors
-api.interceptors.response.use((response) => {
-    return response;
-}, (error) => {
-    if (error.response && error.response.status === 401) {
-        localStorage.removeItem('token');
-        window.location.href = '/login';
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response && error.response.status === 401) {
+            localStorage.removeItem('token');
+            window.location.href = '/login';
+        }
+
+        return Promise.reject(error);
     }
-    return Promise.reject(error);
-});
+);
 
 export default api;
